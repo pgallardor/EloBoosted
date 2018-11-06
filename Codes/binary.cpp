12004736dd13d4d5f0e6d35c98da55108660b978
pgallardor <pedrogallardorobinson@gmail.com>
2018-11-06 17:19:34 -0300

    nuevos códigos

--- Codes/binary.cpp
+++ Codes/binary.cpp
@@ -5,7 +5,7 @@ int binary(int A[], int E, int min, int max){
 		int mid = (min + max)/2;
 		if (A[mid] == E) return mid;
 
-		if (A[mid] > E) min = mid + 1;
+		if (A[mid] < E) min = mid + 1;
 		else max = mid - 1;
 	}
 	return -1;
